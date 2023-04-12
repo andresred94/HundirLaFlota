c0aba8853a79dd0d47fb79ce478f759bee4c4cf6
andresred94 <bravocastro.andresmartin@gmail.com>
2023-04-12 09:08:55 +0200

    Ahora si que se envia los datos

--- src/Casilla.java
+++ src/Casilla.java
@@ -16,11 +16,6 @@ public class Casilla {
     private char barco = 'B';
     private char tocado = 'X';
 
-    //Si modifico este archivo se ve en GitHub?
-    //Sigo intentando ver las modificacioens
-    //otra prueba
-    //otra prueba 3
-
     //metodos constructores
     public Casilla(int fila, int columna, boolean estadoCasilla,boolean impactado,boolean hundido, char agua, char barco){
         setFila(fila);
